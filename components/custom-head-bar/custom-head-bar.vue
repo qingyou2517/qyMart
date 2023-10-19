68df99192ad9ef8dfdbdc7319c51afe363d987b3
qingyou2517 <2517789608@qq.com>
2023-10-20 06:53:21 +0800

    弹出层展示品牌具体信息

--- components/custom-head-bar/custom-head-bar.vue
+++ components/custom-head-bar/custom-head-bar.vue
@@ -17,7 +17,7 @@
 					</navigator>
 				</view>
 				<view class="body" :class="foldState?'fold':''" :style="{height:bodyBarHeight + 'px'}">
-					<view class="brand">
+					<view class="brand" @click="clickBrand">
 						<view class="pic">
 							<image class="img" :src="brandData.thumb[0].url" mode="aspectFill	"></image>
 						</view>
@@ -38,7 +38,37 @@
 				</view>
 			</view>
 		</view>
-		
+
+		<view v-if="brandData.name">
+			<uni-popup ref="brandPopup" type="bottom">
+				<view class="brandWrapper">
+					<view class="close" @click="closeBrandPop">
+						<u-icon name="arrow-down" color="#666"></u-icon>
+					</view>
+
+					<view class="brand">
+						<view class="pic">
+							<image class="img" :src="brandData.thumb[0].url" mode="aspectFill"></image>
+						</view>
+						<view class="title">{{brandData.name}}</view>
+						<view class="des">{{brandData.about}}</view>
+					</view>
+
+					<view class="cell">
+						<u-cell-group>
+							<u-cell icon="phone" :title="brandData.mobile" isLink
+								@click="clickMobile(brandData.mobile)"></u-cell>
+							<u-cell icon="map" :title="brandData.address" isLink @click="clickMap(brandData.address)">
+							</u-cell>
+						</u-cell-group>
+					</view>
+
+				</view>
+
+				<!-- 安全区域 -->
+				<view class="safe-area-bottom"></view>
+			</uni-popup>
+		</view>
 	</view>
 </template>
 
@@ -68,7 +98,44 @@
 		},
 		methods: {
 			// ...mapMutations(['setBrand'])
-			...mapActions(['getBrandData'])
+			...mapActions(['getBrandData']),
+
+			// 点击头部 brand 区域，触发弹出层展示品牌信息
+			clickBrand() {
+				this.$refs.brandPopup.open()
+			},
+
+			// 关闭弹出层
+			closeBrandPop() {
+				this.$refs.brandPopup.close()
+			},
+
+			// 点击弹出层里的地图
+			clickMap(address) {
+				uni.request({
+					url: "https://restapi.amap.com/v3/geocode/geo",
+					data: {
+						key: "01534dc3856ee74996fd84d4fe15ed57",
+						address,
+					},
+					success: res => {
+						// console.log(res)
+						let location = res.data.geocodes[0].location // 字符串：经度,纬度
+						let arr = location.split(',').map(item => Number(item))
+						uni.openLocation({
+							latitude: arr[1], // 纬度：数字
+							longitude: arr[0], // 经度：数字
+						})
+					}
+				})
+			},
+
+			// 点击弹出层里的手机号 
+			clickMobile(mobile) {
+				uni.makePhoneCall({
+					phoneNumber: mobile
+				});
+			},
 		},
 		mounted() {
 
@@ -223,6 +290,56 @@
 					}
 				}
 			}
+		}
+	}
+
+	.brandWrapper {
+		padding: 30rpx;
+		border-radius: 20rpx 20rpx 0 0;
+		background: #fff;
+		@include flex-box();
+		flex-direction: column;
+
+		.close {
+			background: $page-bg-color;
+			padding: 5rpx 20rpx;
+			border-radius: 30rpx;
+		}
+
+		.brand {
+			@include flex-box();
+			flex-direction: column;
+			padding: 30rpx 0;
+
+			.pic {
+				width: 100rpx;
+				height: 100rpx;
+				border: 1px solid $border-color-light;
+				border-radius: 50%;
+				overflow: hidden;
+
+				.img {
+					width: 100%;
+					height: 100%;
+				}
+			}
+
+			.title {
+				font-size: 34rpx;
+				color: $text-font-color-1;
+				padding: 10rpx 0;
+			}
+
+			.des {
+				font-size: 28rpx;
+				color: $text-font-color-2;
+			}
+
+		}
+
+		.cell {
+			width: 100%;
+			padding: 30rpx 0 100rpx;
 		}
 	}
 </style>
