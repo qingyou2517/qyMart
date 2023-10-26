ae54385bba2011a2b33a2f48e5ac17ce1da3bcd4
qingyou2517 <2517789608@qq.com>
2023-10-26 17:09:35 +0800

    完成规格弹窗内的点击判断

--- components/pro-select-specs/pro-select-specs.vue
+++ components/pro-select-specs/pro-select-specs.vue
@@ -13,7 +13,9 @@
 							<view class="list" v-for="(item,itemIndex) in detailData.sku_select">
 								<view class="title">{{ item.skuName }}</view>
 								<view class="group">
-									<view class="btn" v-for="child in item.children">{{ child.name }}</view>
+									<view class="btn" :class="skuArr.includes(child.name)?'active':''"
+										@click="clickSelect(itemIndex,childIndex)"
+										v-for="(child,childIndex) in item.children">{{ child.name }}</view>
 								</view>
 							</view>
 						</view>
@@ -26,7 +28,8 @@
 				</view>
 
 				<view class="footer">
-					<u-button color="#EC544F" icon="shopping-cart" iconColor="#fff" @click="clickConfirm">确认</u-button>
+					<u-button color="#EC544F" icon="shopping-cart" iconColor="#fff" @click="clickConfirm"
+						:disabled="confirmState">确认</u-button>
 				</view>
 			</view>
 		</u-popup>
@@ -44,25 +47,62 @@
 		data() {
 			return {
 				numValue: 1,
+				userSelect: [],
 			};
 		},
 		computed: {
 			...mapGetters(['proSpecsState', 'detailData']),
+
+			// 从 userSelect 里取出二级属性的 name，再维护成一个数组，用于动态绑定class判断
+			skuArr() {
+				return this.userSelect.map(item => item.name)
+			},
+
+			// 确认按钮可否点击
+			confirmState() {
+				// detailData 初始情况下是{}，而在点击"选规格"按钮之后才会写入 vuex
+				if (this.detailData?.sku_select?.length === this.skuArr.length) {
+					return false
+				} else {
+					return true
+				}
+			},
 		},
 		methods: {
 			...mapMutations(['setProSpecsState']),
+			
+			// 规格按钮的点击效果
+			clickSelect(itemIndex, childIndex) {
+			  let obj = {
+			    // 一级属性的 id
+			    id: this.detailData.sku_select[itemIndex]._id,
+			    // 二级属性的 name
+			    name: this.detailData.sku_select[itemIndex].children[childIndex].name
+			  }
+			  // 判断该一级属性下：是否已经点击选中了二级属性
+			  let index = this.userSelect.findIndex(item => {
+			    return item.id === obj.id
+			  })
+			  if (index < 0) {
+			    this.userSelect.push(obj)
+			  } else {
+			    // 若已选，则替换之前选择的二级属性（一个一级属性下，同时只允许选择一个二级属性，且至少选择一个二级属性）
+			    this.userSelect.splice(index, 1, obj)
+			  }
+			},
 
 			// 点击关闭弹窗
 			onClose() {
 				this.setProSpecsState(false)
 				this.numValue = 1
+				this.userSelect = []
 			},
-
+			
 			// 确认购买
 			clickConfirm() {
 				this.onClose()
 			},
-
+			
 			// 修改步进器的值
 			valChange() {
 
