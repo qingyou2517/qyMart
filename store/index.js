be95a8fdfccf219a230945dfc3cfa87ef7f28dfe
qingyou2517 <2517789608@qq.com>
2023-10-29 07:48:11 +0800

    实现vuex数据持久化

--- store/index.js
+++ store/index.js
@@ -1,3 +1,4 @@
+import createPersistedState from 'vuex-persistedstate' // 引入数据持久化插件
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -16,7 +17,17 @@ const store = new Vuex.Store({
 		cars,
 		brand,
 		goods,
-	}
+	},
+	plugins: [
+		createPersistedState({
+			paths: ['cars'],
+			storage: { // 存储方式定义  
+				getItem: (key) => uni.getStorageSync(key), // 获取  
+				setItem: (key, value) => uni.setStorageSync(key, value), // 存储  
+				removeItem: (key) => uni.removeStorageSync(key) // 删除  
+			}
+		})
+	]
 })
 
 export default store
